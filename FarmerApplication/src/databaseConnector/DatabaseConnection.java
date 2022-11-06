4b3e80610e0e4092df19b835355c1ffa8ef7b18a
Yogesh-M2001 <117581100+Yogesh-M2001@users.noreply.github.com>
2022-11-06 20:20:02 +0530

    Add files via upload
    Initial Commit

---
+++ FarmerApplication/src/databaseConnector/DatabaseConnection.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseConnector;
+
+import java.io.IOException;
+import java.sql.*;
+/**
+ *
+ * @author Muthu Pandi
+ */
+public class DatabaseConnection {
+   
+    private Connection connection;
+    
+    public DatabaseConnection(){
+        connection = null;
+    }
+    
+    public void start() throws IOException{
+        
+    }
+}
---
+++ FarmerApplication/src/models/SignupInputModel.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Muthu Pandi
+ */
+public class SignupInputModel {
+    
+    private String name;
+    
+    private String userName;
+    
+    private String mailId;
+    
+    private String phoneNumber;
+    
+    private String password;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the mailId
+     */
+    public String getMailId() {
+        return mailId;
+    }
+
+    /**
+     * @param mailId the mailId to set
+     */
+    public void setMailId(String mailId) {
+        this.mailId = mailId;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
